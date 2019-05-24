aa59f26083416f4356a6dbac80fe669dc2c1696c
Aldrin Tingson <aldrin.tingson@petcircle.com.au>
2019-05-24 10:21:10 +0800

    created unit test for freebie promos

--- FreebiePromo.js
+++ FreebiePromo.js
@@ -1,32 +1,62 @@
-function requirementItemMatchingByCodeReducer(item) {
-    return req => req.code === item.code;
-}
+const requirementItemMatchingByCodeReducer = item => req => req.code === item.code;
 
 export default class FreebiePromo {
 
-    constructor(name, requirements, rewards) {
+    constructor(name = 'Unnamed Freebie Promo', requirements = [], rewards = []) {
         this.name = name;
         this.requirements = requirements;
         this.rewards = rewards;
     }
 
-    checkIfApplicable(items = []) {
+    checkIfApplicable(originalItems = []) {
         if (!this.requirements) {
             return true
         }
 
-        const itemsMatchingRequirements = items.filter(
-            item => this.requirements.filter(requirementItemMatchingByCodeReducer(item)).length > 0
-        );
-        const fulfilledRequirements = itemsMatchingRequirements
-            .map(item => this.requirements.find(requirementItemMatchingByCodeReducer(item)))
-            .filter(req =>
-                req.qty >= itemsMatchingRequirements.find(item => item.code === req.code).qty
-            );
+        const fulfilledRequirements = this.getFulfilledRequirements(originalItems);
         return fulfilledRequirements.length === this.requirements.length;
     }
 
-    apply(items = []) {
+    getFulfilledRequirements(originalItems) {
+        const itemsMatchingRequirements = originalItems.filter(
+            item => this.requirements.filter(requirementItemMatchingByCodeReducer(item)).length > 0
+        );
+        return itemsMatchingRequirements
+            .map(item => this.requirements.find(requirementItemMatchingByCodeReducer(item)))
+            .filter(req =>
+                req.qty <= itemsMatchingRequirements.find(item => item.code === req.code).qty
+            );
+    }
 
+    /**
+     * processes the items by piping them up to promo
+     * @param unprocessedItems the items yet to be processed by this promo
+     * @return copy of processedItems that went through this
+     */
+    apply(unprocessedItems) {
+        const processedItems = JSON.parse(JSON.stringify(unprocessedItems));
+        let freebieCount = 0;
+
+        if (this.checkIfApplicable(unprocessedItems)) {
+            //const rewardsToApplyToWholeCart = this.rewards.filter(reward => !reward.applyPerItem); // TODO
+
+            const fulfilledRequirements = this.getFulfilledRequirements(unprocessedItems);
+            const freebieCountsPerRequirement = fulfilledRequirements.map(req => {
+                const matchingItem = processedItems.find(item => item.code === req.code);
+                return Math.floor(matchingItem.qty / req.qty)
+            });
+            freebieCount = Math.min(...freebieCountsPerRequirement);
+        }
+
+        const freebies = freebieCount > 0 ? this.rewards.map(reward => ({
+            code: reward.code,
+            qty: reward.qty * freebieCount,
+            price: 0
+        })) : [];
+
+        return [
+            ...processedItems,
+            ...freebies
+        ]
     }
 }
--- FreebiePromo.test.js
+++ FreebiePromo.test.js
@@ -1,19 +1,217 @@
 import FreebiePromo from "./FreebiePromo";
 import fs from 'fs'
 
-const rawData = fs.readFileSync("pricing-rules.json");
-const data = JSON.parse(rawData);
+let threeForTwo;
+let TwoGBFreeOneGB;
+beforeEach(() => {
 
-const freebiePromoFromJson = data.autoPromos[0];
+    const rawData = fs.readFileSync("pricing-rules.json");
+    const data = JSON.parse(rawData);
 
-const freebiePromo = new FreebiePromo(freebiePromoFromJson.name, freebiePromoFromJson.requirements, freebiePromoFromJson.rewards);
+    threeForTwo = new FreebiePromo(data.autoPromos[0].name, data.autoPromos[0].requirements, data.autoPromos[0].rewards);
+    TwoGBFreeOneGB = new FreebiePromo(data.autoPromos[2].name, data.autoPromos[2].requirements, data.autoPromos[2].rewards);
+});
 
-test('applicable 3 for 2 deal on Unlimited 1GB shopping cart', () => {
+
+test('shopping cart eligible for 3 for 2 deal on Unlimited 1GB', () => {
     const items = [
         {
             code: 'ult_small',
-            qty: 2
+            qty: 2,
+            price: 24.90
         }
     ];
-    expect(freebiePromo.checkIfApplicable(items)).toBe(true);
+    expect(threeForTwo.checkIfApplicable(items)).toBe(true);
+});
+
+test('shopping cart not eligible for 2 deal on Unlimited 1GB shopping cart', () => {
+    const items = [
+        {
+            code: 'ult_small',
+            qty: 1,
+            price: 24.90
+        }
+    ];
+    expect(threeForTwo.checkIfApplicable(items)).toBe(false);
+});
+
+test('shopping cart got 1 freebie from 3 for 2 deal on Unlimited 1GB', () => {
+    const items = [
+        {
+            code: 'ult_small',
+            qty: 2,
+            price: 24.90
+        }
+    ];
+    const expected = [
+        {
+            code: 'ult_small',
+            qty: 2,
+            price: 24.90
+        },
+        {
+            code: 'ult_small',
+            qty: 1,
+            price: 0
+        }
+    ];
+    expect(threeForTwo.apply(items)).toEqual(expected);
+
+    items[0].qty = 3;
+    expected[0].qty = 3;
+    expect(threeForTwo.apply(items)).toEqual(expected);
+});
+
+test('shopping cart got 3 freebie from 3 for 2 deal on Unlimited 1GB', () => {
+    const items = [
+        {
+            code: 'ult_small',
+            qty: 6,
+            price: 24.90
+        }
+    ];
+    const expected = [
+        {
+            code: 'ult_small',
+            qty: 6,
+            price: 24.90
+        },
+        {
+            code: 'ult_small',
+            qty: 3,
+            price: 0
+        }
+    ];
+    expect(threeForTwo.apply(items)).toEqual(expected);
+});
+
+test('suddenly 3 for 2 deal on Unlimited 1GB requirement increased', () => {
+    threeForTwo.requirements.push({
+        code: 'ult_medium',
+        qty: 1
+    });
+    let items = [
+        {
+            code: 'ult_small',
+            qty: 2,
+            price: 24.90
+        },
+        {
+            code: 'ult_medium',
+            qty: 1,
+            price: 29.90
+        }
+    ];
+    const expected = [
+        {
+            code: 'ult_small',
+            qty: 2,
+            price: 24.90
+        },
+        {
+            code: 'ult_medium',
+            qty: 1,
+            price: 29.90
+        },
+        {
+            code: 'ult_small',
+            qty: 1,
+            price: 0
+        }
+    ];
+    expect(threeForTwo.apply(items)).toEqual(expected);
+
+    // the cart did not satisfy the requirement
+    items = [
+        {
+            code: 'ult_small',
+            qty: 2,
+            price: 24.90
+        }
+    ];
+    expect(threeForTwo.apply(items)).toEqual(items);
+});
+
+test('suddenly 3 for 2 deal on Unlimited 1GB reward increased', () => {
+    threeForTwo.rewards.push({
+        type : "FreebiePromo",
+        applyPerItem: false,
+        code: 'ult_medium',
+        qty: 1,
+    });
+    const items = [
+        {
+            code: 'ult_small',
+            qty: 2,
+            price: 24.90
+        }
+    ];
+    const expected = [
+        {
+            code: 'ult_small',
+            qty: 2,
+            price: 24.90
+        },
+        {
+            code: 'ult_small',
+            qty: 1,
+            price: 0
+        },
+        {
+            code: 'ult_medium',
+            qty: 1,
+            price: 0
+        }
+    ];
+    expect(threeForTwo.apply(items)).toEqual(expected);
+});
+
+test('shopping cart eligible for Buy Unlimited 2GB get 1GB Data-pack free', () => {
+    const items = [
+        {
+            code: 'ult_medium',
+            qty: 1,
+            price: 29.90
+        }
+    ];
+    expect(TwoGBFreeOneGB.checkIfApplicable(items)).toBe(true);
+});
+
+test('shopping cart not eligible for Buy Unlimited 2GB get 1GB Data-pack free', () => {
+    const items = [
+        {
+            code: 'ult_small',
+            qty: 1,
+            price: 24.90
+        }
+    ];
+    expect(TwoGBFreeOneGB.checkIfApplicable(items)).toBe(false);
+});
+
+test('shopping cart got 1 freebie from Buy Unlimited 2GB get 1GB Data-pack free', () => {
+    const items = [
+        {
+            code: 'ult_medium',
+            qty: 1,
+            price: 29.90
+        }
+    ];
+    const expected = [
+        {
+            code: 'ult_medium',
+            qty: 1,
+            price: 29.90
+        },
+        {
+            code: '1gb',
+            qty: 1,
+            price: 0
+        }
+    ];
+    expect(TwoGBFreeOneGB.apply(items)).toEqual(expected);
+
+    items[0].qty = 3;
+    expected[0].qty = 3;
+    expected[1].qty = 3;
+    expect(TwoGBFreeOneGB.apply(items)).toEqual(expected);
 });
